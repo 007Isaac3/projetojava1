829eb3923f886239064a232e0bedfc51b03b9f2f
007Isaac3 <isaac123lima321@gmail.com>
2023-09-12 16:27:48 -0300

    adicionando campo data de nascimento

--- projetojava1/src/cursojava/aleatorio/Pessoa.java
+++ projetojava1/src/cursojava/aleatorio/Pessoa.java
@@ -1,5 +1,7 @@
 package cursojava.aleatorio;
 
+import java.util.Date;
+
 public class Pessoa {
 
 	private String nome;
@@ -8,6 +10,18 @@ public class Pessoa {
 
 	private int idade;
 	
+	private Date dataNascimento;
+	
+	
+	
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
 	public int getIdade() {
 		return idade;
 	}
